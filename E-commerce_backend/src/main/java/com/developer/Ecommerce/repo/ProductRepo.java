3e504625d7e55286b086696d67aed1713cc329d5
daniel <danielgebrey@gmail.com>
2024-07-24 13:15:02 -0500

    My BA final project

--- E-commerce_backend/src/main/java/com/developer/Ecommerce/repo/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.developer.Ecommerce.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.developer.Ecommerce.model.Product;
-
-public interface ProductRepo extends JpaRepository<Product, Long> {
-
-}
